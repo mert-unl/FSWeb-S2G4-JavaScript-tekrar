17c58d801303c07b580b2e923166205c26420706
troemanes <mertunal1998@gmail.com>
2025-02-13 18:07:25 +0300

    görev 1 çemberin çevresi

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ console.log(KareninAlani(10))
 	4. Hesaplanan çemberin çevresi döndürülecektir.
 */
 
+function CemberinCevresi(yarıçap) {
+  return 2 * pi *yarıçap
+}
+
+console.log(CemberinCevresi(5))
+
 /* (Oto test yok) Yukarıdaki CemberinCevresi fonksiyonunu yarıçap = 5 vererek aşağıda çalıştırıp, sonucu konsolda gözlemleyin (console.log)  */
 
 /* 	GÖREV 2:  
